ad128e1c3ada49eee9a5709e9f37d9f5c529d0c2
Beverly Appleton <bfcain19@gmail.com>
2020-03-31 17:02:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def take_a_number(line, new_person)
 end
 
 def now_serving(line)
-  if deli.empty?
+  if line.empty?
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{line[0]}."
